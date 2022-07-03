ac5c7eae7a3a3dddf40125eeb53f2c1c58cd017d
asayad1 <ahmadksayad@gmail.com>
2022-07-03 05:13:28 -0400

    Overhauled API call for a single room

--- Form1.cs
+++ Form1.cs
@@ -9,37 +9,24 @@ namespace UMBC_Laundry
 {
     public partial class Form1 : Form
     {     
-        Rooms rooms;
+        LaundryRoom rooms;
         LaundryList laundry_rooms;
 
 
         public Form1()
         {
-            InitializeComponent();
-            LoadRooms();
+            InitializeComponent();  
             LoadLaundryData();
-
-            GUIControl gui = new GUIControl(this);
-
-            foreach (LaundryRoom room in laundry_rooms.room_list)
-            {
-                gui.CreateGUIElement(APIHelper.abrevs[room.ID], room.available_washers, room.available_dryers);
-            }
         }
 
-        void LoadRooms()
-        {
-            string json = GETRequest(APIHelper.ROOMS_PROJ_KEY);
-            rooms = JsonConvert.DeserializeObject<Rooms>(json); 
-        }
 
         void LoadLaundryData()
         {
-            string json = GETRequest(APIHelper.LAUNDRY_PROJ_KEY);
-            laundry_rooms = JsonConvert.DeserializeObject<LaundryList>(json);
+            string json = GETRequest("484107");
+            rooms = JsonConvert.DeserializeObject<LaundryRoom>(json);
         }
 
-        string GETRequest(string PROJECT_KEY)
+        string GETRequest(string room_loc)
         {
             // Handle decompression whenever we have a GetRequest 
             var clientHandler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate };
@@ -47,7 +34,7 @@ namespace UMBC_Laundry
             // Get JSON response from API 
             using (var client = new HttpClient(clientHandler))
             {
-                var url = APIHelper.FormatURL(PROJECT_KEY);
+                var url = APIHelper.FormatURL(room_loc);
                 var result = client.GetAsync(url).Result;
                 var json = result.Content.ReadAsStringAsync().Result;
                 return json; 
---
+++ Machine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMBC_Laundry
+{
+    internal class Machine
+    {
+        public string appliance_type { get; set; } // Tells us if washer or dryer 
+        public string appliance_desc { get; set; } // Tells us machine number
+        public string time_left_lite { get; set; } // Tells us washer availability
+        public string percentage { get; set; }     // Tells us % done if machine is being used 
+
+        /// <summary>
+        /// If there are double
+        /// </summary>
+        public string appliance_type2 { get; set; } // Tells us if washer or dryer 
+        public string appliance_desc2 { get; set; } // Tells us machine number
+        public string time_left_lite2 { get; set; } // Tells us washer availability
+        public string percentage2 { get; set; }     // Tells us % done if machine is being used 
+
+
+    }
+}
